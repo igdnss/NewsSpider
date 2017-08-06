e75fca32a471b6dbaf2f228b2977e9f078c011cd
chenglin <chenglin02.wang@phicomm.com>
2017-08-07 00:22:42 +0800

    Change the storage path to "testdata/data/tencent/sports/"

--- news_spider/news_spider/spiders/Tencent.py
+++ news_spider/news_spider/spiders/Tencent.py
@@ -9,6 +9,7 @@ import json
 from pip._vendor.requests.packages.urllib3.util import url
 from __builtin__ import str
 import sys
+import os
 
 class TencentSpider(scrapy.Spider):
 
@@ -33,14 +34,13 @@ class TencentSpider(scrapy.Spider):
 		if len(day) < 2:
 			day = '0'+day
 		return 'a/'+year+month+day
-	#===========================================================================
-	# def getTimeStr(self,content):
-	# 	bracePos = content.index('{')-1
-	# 	tempData = content[bracePos:]
-	# 	pubTimeTail = tempData.index('pubtime')+len('pubtime')+2
-	# 	timeStr = tempData[pubTimeTail:pubTimeTail+16]
-	# 	return timeStr
-	#===========================================================================
+	#get the time from news page in string
+	def getTimeStr(self,content):
+		bracePos = content.index('{')-1
+		tempData = content[bracePos:]
+		pubTimeTail = tempData.index('pubtime')+len('pubtime')+2
+		timeStr = tempData[pubTimeTail:pubTimeTail+16]
+		return timeStr
 	    
 	def parse(self,response):
 		url = self.base_url
@@ -51,6 +51,7 @@ class TencentSpider(scrapy.Spider):
 	
 	def parseList(self,response):
 		urls = response.xpath("//a/@href").extract()
+# 		temp="20170806" will be used at middle night
 		temp = self.generateSubPath()
 		print("===================================")
 		for url in urls:
@@ -69,22 +70,27 @@ class TencentSpider(scrapy.Spider):
 		content = response.xpath("//div[@id='Cnt-Main-Article-QQ']/p[@style='TEXT-INDENT: 2em']/text()").extract()
 		cc=''
 		if len(content)>0:
-			fileTitle = response.url[-10:-4]+".txt";
-			file = open(fileTitle,"w") 
+			current_dir = os.path.dirname(__file__)
+			fileTitle = response.url[-10:-4]+".txt"
+			path_prefix = "../../../testdata/data/"
 			scriptCnt = response.xpath("//script[1]/text()").extract()
 			url = response.url
 			title = response.xpath("//title/text()").extract()
 			for c in content:
 				cc = cc+c+'\n'
 			content = cc.strip()
-			position = scriptCnt[0].index('{')-1
-			data = scriptCnt[0][position:]
-			position = data.index('pubtime')+len('pubtime')+2
-			time = data[position:position+16]
-			file.write("\""+"url"+"\""+":"+"\""+url+"\""+'\n') 
-			file.write("\""+"time"+"\""+":"+"\""+time+"\""+'\n') 
-			title = u''.join(title[0]).encode('utf-8')
-			content = u''.join(content).encode('utf-8')
-			file.write("\""+"title"+"\""+":"+"\""+title+"\""+'\n') 
-			file.write("\""+"content"+"\""+":"+"\""+content+"\""+'\n') 
-			file.close()
+			time = self.getTimeStr(scriptCnt[0])
+			
+			if(url.find("sports.qq.com")>=0):
+				#TODO: to abstract the following code to a method
+				rel_path = path_prefix+"tencent/sports/"+fileTitle
+				abs_file_path = os.path.join(current_dir, rel_path)
+				file = open(abs_file_path,"w") 
+				file.write("\""+"url"+"\""+":"+"\""+url+"\""+'\n') 
+				file.write("\""+"time"+"\""+":"+"\""+time+"\""+'\n') 
+				title = u''.join(title[0]).encode('utf-8')
+				content = u''.join(content).encode('utf-8')
+				file.write("\""+"title"+"\""+":"+"\""+title+"\""+'\n') 
+				file.write("\""+"content"+"\""+":"+"\""+content+"\""+'\n') 
+				file.close()
+			
