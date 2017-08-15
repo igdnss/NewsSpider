8581ea9d91cb3c0b1fc159b2981f53bc6d2e4a32
chenglin <chenglin02.wang@phicomm.com>
2017-08-15 19:13:47 +0800

    1, fixed the bug of parsing time of finance news 2, improved the crawling efficiency

--- news_spider/news_spider/spiders/Tencent.py
+++ news_spider/news_spider/spiders/Tencent.py
@@ -10,6 +10,8 @@ from pip._vendor.requests.packages.urllib3.util import url
 from __builtin__ import str
 import sys
 import os
+import logging
+
 
 class TencentSpider(scrapy.Spider):
 
@@ -26,6 +28,13 @@ class TencentSpider(scrapy.Spider):
 	
 	fileName = ''
 	path_prefix = '../../../testdata/data/'
+	
+	logging.basicConfig(filename='example.log',level=logging.INFO)
+	
+	startCrawl = datetime.datetime.now()
+	logging.info("~~~~~~~~~~~~~~~~~~startCrawl at : "+str(startCrawl))
+# 	print ("~~~~~~~~~~~~~~~~~~startCrawl at : "+str(startCrawl))
+	
 
 	def generateSubPath(self):
 		currentTime = datetime.datetime.now()
@@ -40,18 +49,9 @@ class TencentSpider(scrapy.Spider):
 	
 	#get the time from news page in string
 	def getTimeStr(self,content):
-# 				TODO: fix bug: has an error when extract pubtime
 		bracePos = content.index('{')-1
 		tempData = content[bracePos:]
 		pubTimeTail = tempData.index('pubtime')+len('pubtime')+2
-		#=======================================================================
-		# try:
-		# 	pubTimeTail = tempData.index('pubtime')+len('pubtime')+2
-		# except ValueError,e:
-		# 	print 'pubtime: '+e
-		# finally:
-		# 	quit()
-		#=======================================================================
 		timeStr = tempData[pubTimeTail:pubTimeTail+16]
 		return timeStr
 	    
@@ -62,7 +62,7 @@ class TencentSpider(scrapy.Spider):
 
 	
 	def parseList(self,response):
-		print("--------------parsing list--------------")
+		logging.info("--------------parsing list--------------")
 		urls = response.xpath("//a/@href").extract()
 # 		temp="20170806" #will be used at middle night
 		temp = self.generateSubPath()
@@ -74,14 +74,6 @@ class TencentSpider(scrapy.Spider):
 			elif(url.find('http://finance.qq.com/')>=0 and len(url)==22):
 				print("--------------finance--------------"+url)
 				yield scrapy.Request(url,self.parseType,dont_filter=True)
-			
-			#===================================================================
-			# if(url.find('http://finance.qq.com/')>=0 and len(url)==22):
-			# 	print("--------------finance--------------"+url)
-			# 	yield scrapy.Request(url,self.parseType,dont_filter=True)	
-			#===================================================================
-				
-				
 			elif(url.find('http://ent.qq.com/')>=0 and len(url)==18):
 				print("--------------ent--------------"+url)
 				yield scrapy.Request(url,self.parseType,dont_filter=True)
@@ -101,43 +93,89 @@ class TencentSpider(scrapy.Spider):
 				print("--------------cul--------------"+url)
 				yield scrapy.Request(url,self.parseType,dont_filter=True)
 			
+			#===================================================================
+			# if(url.find('http://ent.qq.com/')>=0 and len(url)==18):
+			# 	logging.info("--------------finance--------------"+url)
+			# 	print("--------------finance--------------"+url)
+			# 	yield scrapy.Request(url,self.parseType,dont_filter=True)	
+			#===================================================================
+				
+				
 	# parse kinds of type of news
 	def parseType(self,response):
-		print("--------------parsing type--------------")
+		logging.info("--------------parsing type--------------")
+# 		print("--------------parsing type--------------")
+		startparseOneSetNews = datetime.datetime.now()
+		logging.info("@@@@@@@@@@@@@@@@@@@startparseOneSetNews at : "+str(startparseOneSetNews))
+# 		print ("@@@@@@@@@@@@@@@@@@@startparseOneSetNews at : "+str(startparseOneSetNews))
 		urls = response.xpath("//a/@href").extract()
 		temp = self.generateSubPath()
+
+		#=======================================================================
+		# url = 'http://finance.qq.com/a/20170809/010566.htm';
+		# yield scrapy.Request(url,self.parseNews,dont_filter=True)
+		#=======================================================================
 		for url in urls:
 			if 'http' in url or temp in url:
 				url = url 
 				if 'http' in url and temp in url:
-					print("--------------with http url--------------"+url)
+					logging.info("--------------with http url--------------"+url)
+# 					print("--------------with http url--------------"+url)
 					yield scrapy.Request(url,self.parseNews,dont_filter=True)
 				elif temp in url:
 					url = response.url+url
-					print("--------------with temp url--------------"+url)
+					logging.info("--------------with temp url--------------"+url)
+# 					print("--------------with temp url--------------"+url)
 					yield scrapy.Request(url,self.parseNews,dont_filter=True)
+		endparseOneSetNews = datetime.datetime.now()
+		logging.info("@@@@@@@@@@@@@@@@@@@endparseOneSetNews at : "+str(endparseOneSetNews))
+# 		print ("@@@@@@@@@@@@@@@@@@@endparseOneSetNews at : "+str(endparseOneSetNews))
+		logging.info("@@@@@@@@@@@@@@@@@@@one set news cost time : "+str((endparseOneSetNews-startparseOneSetNews).seconds))
+# 		print ("@@@@@@@@@@@@@@@@@@@one set news cost time : "+str((endparseOneSetNews-startparseOneSetNews).seconds))
 			
 	def parseNews(self,response):
-		print("--------------parsing news--------------")
+		logging.info("--------------parsing news--------------")
+# 		print("--------------parsing news--------------")
+		startparseSingleNews = datetime.datetime.now()
+		logging.info("$$$$$$$$$$$$$$$$$$$$startparseSingleNews at : "+str(startparseSingleNews))
+# 		print ("$$$$$$$$$$$$$$$$$$$$startparseSingleNews at : "+str(startparseSingleNews))
 		data = response.xpath("//div[@id='Cnt-Main-Article-QQ']")
 		item = NewsSpiderItem()
 		timee = data.xpath("//span[@class='article-time']/text()").extract()
+		# 修改抽取内容  条件或
 		content = response.xpath("//div[@id='Cnt-Main-Article-QQ']/p[@style='TEXT-INDENT: 2em']/text()").extract()
 		cc=''
 		if len(content)>0:
 			self.fileName = response.url[-10:-4]+".txt" 
-			scriptCnt = response.xpath("//script[1]/text()").extract()
+# 			scriptCnt = response.xpath("//script[1]/text()").extract()
+			scripts = response.xpath("//script/text()").extract()
 			url = response.url
 			title = response.xpath("//title/text()").extract()
 			for c in content:
 				cc = cc+c+'\n'
 			content = cc.strip()
-			time = self.getTimeStr(scriptCnt[0])
-			print("--------------questions urls--------------"+response.url)
+			logging.info("--------------pre url --------------"+response.url)
+			logging.info("--------------pre content --------------"+content)
+			for scriptCnt in scripts:
+				if(scriptCnt.find('pubtime')>0):
+					time = self.getTimeStr(scriptCnt)
+					logging.info("--------------time--------------"+time)
+# 					print("--------------time--------------"+time)
+					break
+# 				time = self.getTimeStr(scriptCnt)
+				#===============================================================
+				# if(scriptCnt.find('pubtime') >= 0):
+				# 	break
+				#===============================================================
+# 			time = self.getTimeStr(scriptCnt[0])
+			logging.info("--------------questions urls--------------"+response.url)
+# 			print("--------------questions urls--------------"+response.url)
 			title = u''.join(title[0]).encode('utf-8')
-			print("--------------questions title--------------"+title)
+			logging.info("--------------questions title--------------"+title)
+# 			print("--------------questions title--------------"+title)
 			content = u''.join(content).encode('utf-8')
-			print("--------------content title--------------"+content)
+			logging.info("--------------content title--------------"+content)
+# 			print("--------------content title--------------"+content)
 			if(len(content) > 0):
 				if(url.find("sports.qq.com") >= 0):
 					self.save("tencent/sports/", url, time, title, content)
@@ -155,17 +193,33 @@ class TencentSpider(scrapy.Spider):
 					self.save("tencent/fashion/", url, time, title, content)
 				elif(url.find("cul.qq.com") >= 0):
 					self.save("tencent/cul/", url, time, title, content)
+				
+				
+				#===============================================================
+				# if(url.find("finance.qq.com") >= 0 or url.find("money.qq.com") >= 0 or url.find("stock.qq.com") >= 0):
+				# 	self.save("tencent/finance/", url, time, title, content)
+				#===============================================================
+					
+					
+		endparseSingleNews = datetime.datetime.now()
+		logging.info("$$$$$$$$$$$$$$$$$$$$ endparseSingleNews at : "+str(endparseSingleNews))
+		logging.info("$$$$$$$$$$$$$$$$$$$$ single news cost time : "+str(endparseSingleNews-startparseSingleNews))
+# 		print ("$$$$$$$$$$$$$$$$$$$$ endparseSingleNews at : "+str(endparseSingleNews))
+# 		print ("$$$$$$$$$$$$$$$$$$$$ single news cost time : "+str(endparseSingleNews-startparseSingleNews))
 				 
 	def save(self,newsType,newsUrl,newsTime,newsTitle,newsContent):
-		print("--------------saving file--------------")
-		current_dir = os.path.dirname(__file__)
-		rel_path = self.path_prefix+newsType+self.fileName
-		abs_file_path = os.path.join(current_dir, rel_path)
-		print("--------------******************abs_file_path--------------"+abs_file_path)
-		file = open(abs_file_path,"w") 
-		file.write("\""+"url"+"\""+":"+"\""+newsUrl+"\""+'\n') 
-		file.write("\""+"time"+"\""+":"+"\""+newsTime+"\""+'\n') 
-		file.write("\""+"title"+"\""+":"+"\""+newsTitle+"\""+'\n') 
-		file.write("\""+"content"+"\""+":"+"\""+newsContent+"\""+'\n') 
-		file.close()
+		 logging.info("--------------saving file--------------")
+		 current_dir = os.path.dirname(__file__)
+		 rel_path = self.path_prefix+newsType+self.fileName
+		 abs_file_path = os.path.join(current_dir, rel_path)
+		 logging.info("--------------******************abs_file_path--------------"+abs_file_path)
+		 file = open(abs_file_path,"w") 
+		 file.write("\""+"url"+"\""+":"+"\""+newsUrl+"\""+'\n') 
+		 file.write("\""+"time"+"\""+":"+"\""+newsTime+"\""+'\n') 
+		 file.write("\""+"title"+"\""+":"+"\""+newsTitle+"\""+'\n') 
+		 file.write("\""+"content"+"\""+":"+"\""+newsContent+"\""+'\n') 
+ 		 file.close()
+		
+	endCrawl = datetime.datetime.now()
+	logging.info("~~~~~~~~~~~~~~~~~~endCrawl at : "+str(endCrawl))
 			
